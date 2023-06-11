cfffad5793c0d2d5760bf605a96da3f07c8096f0
Jahid1604091 <jh409780@gmail.com>
2023-06-11 15:33:53 +0600

    crud done with reredner state

--- src/features/todos/TodoList.jsx
+++ src/features/todos/TodoList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useAddTodoMutation, useDeleteTodoMutation, useGetTodosQuery, useUpdateTodoMutation } from "../api/apiSlice";
-
+import {FaTrashAlt,FaAd} from 'react-icons/fa';
 export default function TodoList() {
   const [newTodo, setNewTodo] = useState("");
   const {data:todos,isError,isLoading,isSuccess,error} = useGetTodosQuery();
@@ -12,7 +12,7 @@ export default function TodoList() {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodo({userId:4,id:5,title:newTodo,completed:false});
+    addTodo({userId:6,id:6,title:newTodo,completed:false});
     setNewTodo("");
   };
   const todoForm = (
@@ -25,8 +25,8 @@ export default function TodoList() {
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
         />
+      <button type="submit"><FaAd/></button>
       </div>
-      <button type="submit">Add Todo</button>
     </form>
   );
 
@@ -36,7 +36,20 @@ export default function TodoList() {
     items = <p>Loading...</p>
   }
   else if(isSuccess){
-    items = JSON.stringify(todos)
+    // items = JSON.stringify(todos)
+    items = todos.map(todo=>{
+        return <article key={todo.id}>
+            <div className="todo">
+                <input type="checkbox"
+                checked={todo?.completed}
+                id={todo.id}
+                onChange={()=>updateTodo({...todo,completed:!todo.completed})}
+                />
+                <label htmlFor="todoTitle">{todo.title}</label>
+                <button onClick={()=>deleteTodo(todo.id)}>{<FaTrashAlt/>}</button>
+            </div>
+        </article>
+    })
   }
   else if(isError){
     items = <p>{error}</p>
